e251499521fc5a6dfdedff8b8ad5d43b9d5ad529
iayasin771@gmail.com <iayasin771@gmail.com>
2025-01-02 20:54:57 -0500

    Delete copy constructor and assignment operator

--- smart_pointers_impl/unique_ptr.h
+++ smart_pointers_impl/unique_ptr.h
@@ -6,7 +6,16 @@ class custom_unique_ptr {
 public:
   custom_unique_ptr() : data(nullptr), deleter(Deleter()) {}
   custom_unique_ptr(std::nullptr_t) : data(nullptr), deleter(Deleter()) {}
-  custom_unique_ptr(Type *pointer) : data(pointer), deleter(Deleter()) {}
+  custom_unique_ptr(Type *pointer, Deleter deleter_in)
+      : data(pointer), deleter(deleter_in) {}
+
+  // delete the reference operator
+  custom_unique_ptr(custom_unique_ptr &other) = delete;
+
+  // delete the copy assignment operator
+  custom_unique_ptr operator=(custom_unique_ptr rhs) = delete;
+
+  // Need to implement move semantics
 
   Type *get() { return data; }
 
