7c9958537c139398a4118eeba373cca0974ea574
iayasin771@gmail.com <iayasin771@gmail.com>
2025-01-13 23:42:40 -0500

    Update size

--- vector_impl/test.cpp
+++ vector_impl/test.cpp
@@ -17,6 +17,7 @@ int main() {
   std::cout << "Size of vector<bool>: " << sizeof(boolVec) << " bytes\n";
   std::cout << "Size of vector<double>: " << sizeof(doubleVec) << " bytes\n";
   std::cout << "Size of vector<char>: " << sizeof(charVec) << " bytes\n";
+  std::cout << "Size of Vector<int>: " << sizeof(myIntVec) << " bytes\n";
 
   return 0;
 }
