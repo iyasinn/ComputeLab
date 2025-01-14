4ad5aed49cddbdc06896f48a61a92b4a2cbb0cec
iayasin771@gmail.com <iayasin771@gmail.com>
2025-01-13 23:59:58 -0500

    Add in fast vector

--- vector_impl/vector.h
+++ vector_impl/vector.h
@@ -1,11 +1,33 @@
-
-
 #include <cstddef>
 template <typename T> class Vector {
 
 public:
+  Vector() {}
+
 private:
+  static const size_t INITIAL_SIZE = 8;
+
   T *_data;
   size_t _capacity;
   size_t _size;
+};
+
+template <typename T> class FastVector {
+
+public:
+  FastVector() {}
+
+private:
+  static const size_t INITIAL_SIZE = 8;
+
+  size_t get_size() {
+    return metadata & 0xFFFF; // Extract lower 16 bits
+  }
+
+  size_t get_capacity() {
+    return (metadata >> 16) & 0xFFFF; // Extract upper 16 bits
+  }
+
+  T *_data;
+  size_t metadata;
 };
